c2d24daa1d98f7cb30da4bab7b3c485ba88f176b
Andrei <andrei_zhigachev@bk.ru>
2022-08-03 11:26:47 +0300

    fixed UserData, added children prop

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,17 +1,33 @@
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText} from '@mui/material';
+import { Avatar, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography} from '@mui/material';
 import React from 'react';
 
 const UserData = (props) => {
-    const {user, postDate} = props;
+    const {user, postDate, children} = props;
     return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} disablePadding>
             <ListItem alignItems="flex-start">
                 <ListItemAvatar>
                     <Avatar alt={user.name} src={user.avatarUrl} />
                 </ListItemAvatar>
                 <ListItemText
                 primary={user.name}
-                secondary={postDate}
+                secondary={
+                    <>
+                        {postDate}
+                        {children && 
+                            <div>
+                                <Typography 
+                                            variant='body2' 
+                                            color='text.primary' 
+                                            mt={1}
+                                            component='span'
+                                        >
+                                            {children}
+                                        </Typography>
+                            </div>
+                        }
+                    </>
+                }
                 />
             </ListItem>
         </List>
