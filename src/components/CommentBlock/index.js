7829a3fa49646901ab38e3818006c3bd6ab30992
Andrei <andrei_zhigachev@bk.ru>
2022-08-03 11:23:23 +0300

    added CommentBlock component

---
+++ src/components/CommentBlock/index.js
@@ -0,0 +1,30 @@
+import { Divider, List, Typography } from '@mui/material';
+import React from 'react';
+import CommentItem from '../CommentItem';
+
+const CommentBlock = ({comments, isLoading}) => {
+
+    return (
+        <>
+            <Typography variant='h4'>Комментарии:</Typography>
+            <List>
+                {(isLoading ? [...Array(5)] : comments).map((comment, i, arr) => (
+                    isLoading ? (
+                        <CommentItem key={i} isLoading />
+                    ) : (
+                        <div key={i}>
+                            <CommentItem 
+                                text={comment.text} 
+                                author={comment.author} 
+                                date={comment.createAt}
+                            />
+                            {arr[i+1] && <Divider variant="inset" component='li' />}
+                        </div>
+                    )
+                ))}
+            </List>
+        </>
+    );
+};
+
+export default CommentBlock;
