0edd1aa4b7e9e82ce5d4cb0f8af16dd0e059dbb8
Andrei <andrei_zhigachev@bk.ru>
2022-08-20 20:24:32 +0300

    added login page layout

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import {useState} from 'react';
+import { Button, Container, IconButton, Stack, TextField, Typography } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        password: '',
+        email: '',
+        showPassword: false
+    });
+
+    const handleClickShowPassword = () => {
+        setFormData(formData => ({
+            ...formData,
+            showPassword: !formData.showPassword,
+        }));
+    };
+    
     return (
-        <div>
-            login
-        </div>
+        <Container
+            maxWidth='xs' 
+            sx={{mt: 10}}
+        >
+            <form>
+                <Stack
+                    alignItems='center'
+                    spacing={2}
+                >
+                    <Typography
+                        variant='h4'
+                    >
+                        Авторизация
+                    </Typography>
+                    <TextField 
+                        label="Введите ваш email" 
+                        variant="outlined"
+                        color='secondary' 
+                        type='email'
+                        fullWidth 
+                        autoFocus
+                    />
+                    <TextField 
+                        label="Введите ваше пароль" 
+                        variant="outlined" 
+                        color='secondary'
+                        type={formData.showPassword ? 'text' : 'password'}
+                        InputProps={{
+                            endAdornment: formData.showPassword ? <IconButton onClick={handleClickShowPassword}><VisibilityOffIcon /></IconButton> 
+                                                                : <IconButton onClick={handleClickShowPassword}><VisibilityIcon /></IconButton>,
+                          }}
+                        fullWidth
+                    />
+                    <Button
+                        variant='contained'
+                        color='secondary'
+                        fullWidth
+                    >
+                        Войти
+                    </Button>
+                </Stack>
+            </form>
+        </Container>
     );
 };
 
