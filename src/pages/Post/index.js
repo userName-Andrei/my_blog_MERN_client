bc37967a98593445521420c20e458ff631782302
Andrei <andrei_zhigachev@bk.ru>
2022-08-30 19:14:47 +0300

    added Skeleton when Post page have status loading

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -8,7 +8,7 @@ import Post from '../../components/Post';
 import dateFixer from '../../utils/dateFixer';
 import { useDispatch, useSelector } from 'react-redux';
 import { isAuthChecker } from '../../store/slices/authSlice';
-import { fetchCommentsByPostId, fetchPostByPostId } from '../../store/slices/postSlice';
+import { clearPosts, fetchCommentsByPostId, fetchPostByPostId } from '../../store/slices/postSlice';
 
 const FullPost = () => {
     const dispatch = useDispatch();
@@ -20,24 +20,33 @@ const FullPost = () => {
     const userData = useSelector(state => state.auth.user);
 
     useEffect(() => {
+        dispatch(clearPosts())
         dispatch(fetchPostByPostId(postId))
         dispatch(fetchCommentsByPostId(postId))
     }, []);
 
     return (
         <Stack mt={10} mb={4}>
-            <Post 
-                user={post.author} 
-                image={post.previewUrl}
-                postDate={dateFixer(post.createdAt)}
-                title={post.title}
-                tags={post.tags}
-                text={<ReactMarkdown children={post.text} />}
-                views={post.viewsCount}
-                comments={post.commentCount}
-                isLoading={postStatus === 'loading'}
-                isFullPost
-            />
+            {!post
+            ?
+                <Post 
+                    isLoading={postStatus === 'loading'}
+                    isFullPost
+                />
+            :
+                <Post 
+                    user={post.author} 
+                    image={post.previewUrl}
+                    postDate={dateFixer(post.createdAt)}
+                    title={post.title}
+                    tags={post.tags}
+                    text={<ReactMarkdown children={post.text} />}
+                    views={post.viewsCount}
+                    comments={post.commentCount}
+                    isLoading={postStatus === 'loading'}
+                    isFullPost
+                />
+            }
             <CommentBlock comments={comments.items} isLoading={comments.status === 'loading'}/>
             {comments.status === 'error' && <Typography variant='body1' color='error' ml={2} mb={2}>Произошла ошибка, попробуйте перезагрузить страницу</Typography>}
             {isAuth && <AddComment user={userData}/>}
