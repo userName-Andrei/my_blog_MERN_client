9371e29c03eabdf22c5e07514e4b59c89ecd735a
Andrei <andrei_zhigachev@bk.ru>
2022-08-30 19:16:35 +0300

    added theme of blog

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,4 @@
-import { Container } from '@mui/system';
-import {useEffect} from 'react';
+import {useEffect, Suspense, lazy} from 'react';
 import {
     BrowserRouter,
     Routes,
@@ -8,13 +7,16 @@ import {
 import { useDispatch } from 'react-redux';
 import { fetchAuth } from '../store/slices/authSlice';
 import Header from '../components/Header';
-import {
-    Main,
-    FullPost,
-    AddPost,
-    Login,
-    Register
-} from '../pages';
+
+import { Container, ThemeProvider } from '@mui/material';
+import theme from '../theme';
+
+
+const Main = lazy(() => import('../pages/Main'));
+const FullPost = lazy(() => import('../pages/Post'));
+const AddPost = lazy(() => import('../pages/AddPost'));
+const Login = lazy(() => import('../pages/Login'));
+const Register = lazy(() => import('../pages/Register'));
 
 function App() {
     const dispatch = useDispatch();
@@ -26,19 +28,23 @@ function App() {
     return (
         <>
             <BrowserRouter>
-                <Header/>
-                <Container maxWidth="lg">
-                    <Routes>
-                        <Route path='/' element={<Main/>} />
-                        <Route path='*' element={<Main/>} />
-                        <Route path='/posts/tags/:tag' element={<Main/>} />
-                        <Route path='/posts/:id' element={<FullPost/>} />
-                        <Route path='/posts/:id/edit' element={<AddPost/>} />
-                        <Route path='/posts/add-post' element={<AddPost/>} />
-                        <Route path='/login' element={<Login/>} />
-                        <Route path='/register' element={<Register/>} />
-                    </Routes>
-                </Container>
+                <ThemeProvider theme={theme}>
+                    <Header/>
+                    <Suspense>
+                        <Container maxWidth="lg">
+                            <Routes>
+                                <Route path='/' element={<Main/>} />
+                                <Route path='*' element={<Main/>} />
+                                <Route path='/posts/tags/:tag' element={<Main/>} />
+                                <Route path='/posts/:id' element={<FullPost/>} />
+                                <Route path='/posts/:id/edit' element={<AddPost/>} />
+                                <Route path='/posts/add-post' element={<AddPost/>} />
+                                <Route path='/login' element={<Login/>} />
+                                <Route path='/register' element={<Register/>} />
+                            </Routes>
+                        </Container>
+                    </Suspense>
+                </ThemeProvider>
             </BrowserRouter>
         </>
     );
