c8c2c3c6a3fd7b80f4b7decbb16812ee2accfca5
Andrei <andrei_zhigachev@bk.ru>
2022-09-02 12:28:10 +0300

    fixed request server link

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // пользовательская конфигурация
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://my-blog-serverpart.herokuapp.com/'
 });
 
 // перехват запроса перед отправкой на сервер и добавление в заголовок токена
